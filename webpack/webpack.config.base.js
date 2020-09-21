cd2a564728be0f79f33f13431cd8e12b36915652
isJaakko <1512803764@qq.com>
2020-09-22 00:23:46 +0800

    style: 格式化文件

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -1,39 +1,39 @@
 /** @format */
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const baseConfig = {
-    mode: 'development',
-    entry: path.resolve(__dirname, '../src'),
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, '../dist'),
-    },
-    resolve: {
-        // Add `.ts` and `.tsx` as a resolvable extension.
-        extensions: ['.ts', '.tsx', '.js', 'jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js(x?)$/,
-                exclude: /(node_modules|bower_components)/,
-                use: 'babel-loader',
-            },
-            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: path.resolve(__dirname, '../static/index.html'),
-        }),
+  mode: 'development',
+  entry: path.resolve(__dirname, '../src'),
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js', 'jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js(x?)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: 'babel-loader',
+      },
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, '../static/index.html'),
+    }),
+  ],
 };
 
 module.exports = baseConfig;
--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,14 +1,14 @@
 /** @format */
 
-const path = require('path')
-const baseConfig = require('./webpack.config.base')
+import path from 'path';
+import baseConfig from './webpack.config.base';
 
 module.exports = {
-    ...baseConfig,
-    devServer: {
-        contentBase: path.join(__dirname, '../dist'), // 监听位置
-        port: 9000, // 设置服务器端口
-        open: true,
-        historyApiFallback: true, // 将所有 404 都返回 index.html
-    },
-}
+  ...baseConfig,
+  devServer: {
+    contentBase: path.join(__dirname, '../dist'), // 监听位置
+    port: 9000, // 设置服务器端口
+    open: true,
+    historyApiFallback: true, // 将所有 404 都返回 index.html
+  },
+};
